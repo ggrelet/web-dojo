1ec2c1f40fd415b57f8944908a43e9d816ae2e93
ggrelet <ggrelet@users.noreply.github.com>
2017-05-30 21:24:43 +0200

    Ajout de l'authentification

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,9 +1,16 @@
+var chatService = require('../server/chatService');
+
 var express = require('express');
 var router = express.Router();
 
 /* GET hello world page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+	if (chatService.authenticate(req)) {
+		res.send(1613044729);
+	}
+	else {
+		res.status(401).send();
+	}
 });
 
 module.exports = router;
